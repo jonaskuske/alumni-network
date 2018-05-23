56af534eb52131eda4b88fec94351ed087e411b0
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-05-23 15:47:39 +0200

    feat: add basic form validation for signup

--- src/views/Splash/Signup.vue
+++ src/views/Splash/Signup.vue
@@ -6,9 +6,9 @@
     </section>
     <template v-else>
     <section class="login__section login__section-welcome">
-      <h1 class="login__heading"><span>Nur vier Schritte bis zur</span>Registrierung</h1>
+      <h1 class="login__heading"><span>In fünf Schritten zur</span>Registrierung</h1>
       <p class="login__description">
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus dolor animi et fugit incidunt dignissimos fugiat distinctio omnis iusto, ea cum veniam laudantium natus libero deserunt aspernatur! Consequatur.
+Gründe warum Registrierung nötig, qualitative Inhalte, moderierte Community. Animi et fugit incidunt dignissimos fugiat distinctio omnis iusto, ea cum veniam laudantium natus libero deserunt aspernatur! Consequatur.
       </p>
       <h2 class="login__features-title">Wir benötigen</h2>
       <ul class="login__features-list">
@@ -21,19 +21,48 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus d
     </section>
     <section class="login__section login__section-login">
       <form method="post" @submit.prevent="signup" class="login__form" autocomplete="off">
-        <labelled-input v-model="userdata.name" label="Name" class="login__form-input" />
-        <labelled-input v-model="userdata.mail" label="Email" class="login__form-input" />
-        <labelled-input v-model="userdata.username" label="Nutzername" class="login__form-input" />
-        <labelled-input v-model="userdata.password" label="Passwort" class="login__form-input" />
+        <labelled-input
+          v-model="userdata.name"
+          label="Name"
+          class="login__form-input"
+          :class="[{'login__form-input--active': userdata.name}, {'login__form-input--danger': showLoginWarning && !userdata.name}]"
+        />
+        <labelled-input
+          v-model="userdata.mail"
+          label="Email"
+          type="email"
+          class="login__form-input"
+          :class="[{'login__form-input--active': userdata.mail}, {'login__form-input--danger': showLoginWarning && !userdata.mail}]"
+        />
+        <labelled-input
+          v-model="userdata.username"
+          label="Nutzername"
+          class="login__form-input"
+          :class="[{'login__form-input--active': userdata.username}, {'login__form-input--danger': showLoginWarning && !userdata.username}]"
+        />
+        <labelled-input
+          v-model="userdata.password"
+          label="Passwort"
+          type="password"
+          class="login__form-input"
+          :class="[{'login__form-input--active': userdata.password}, {'login__form-input--danger': showLoginWarning && !userdata.password}]"
+        />
+        <labelled-input
+          v-model="passwordConfirm"
+          label="Passwort bestätigen"
+          type="password"
+          class="login__form-input"
+          :class="[{'login__form-input--active': passwordConfirm}, {'login__form-input--danger': (showLoginWarning && !passwordConfirm) || (passwordConfirm && !passwordValid)}]"
+        />
         <div class="signup__container">
           <label>
             Nutzergruppe
-          <select v-model="userdata.usergroup">
-            <option disabled>Eine Option auswählen...</option>
-            <option>Studierende</option>
-            <option>Alumni</option>
-            <option>Dozenten</option>
-            <option>Gäste</option>
+          <select v-model="userdata.usergroup" :class="{'login__form-input--danger':showLoginWarning && !userdata.usergroup}">
+            <option value="" disabled>Eine Option auswählen...</option>
+            <option value="Studierende">Studierende</option>
+            <option value="Alumni">Alumni</option>
+            <option value="Dozenten">Dozenten</option>
+            <option value="Gäste">Gäste</option>
           </select>
             </label>
           <gallery-thumbnail
@@ -49,6 +78,10 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus d
           <button
             type="submit"
             class="button-main login__button"
+            :class="{'button--danger': redLoginButton}"
+            :disabled="isDisabled"
+            @mouseenter.once="showLoginWarning = true"
+            @touchstart.once="showLoginWarning = true"
           >
             Absenden
           </button>
@@ -56,13 +89,13 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus d
           </div>
       </form>
           <input type="file" accept="image/*" ref="picture" @change="addProfilePicture" class="hide">
-
     </section>
     </template>
   </main>
 </template>
 
 <script>
+  import { wait } from "@/lib/helpers";
   import { mapActions } from "vuex";
   import GalleryThumbnail from "@/components/GalleryThumbnail";
   import LabelledInput from "@/components/LabelledInput";
@@ -85,8 +118,29 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus d
           password: "",
           usergroup: ""
         },
-        loginSuccessful: false
+        passwordConfirm: "",
+        loginSuccessful: false,
+        redLoginButton: false,
+        allowWarn: false,
+        showLoginWarning: false
       };
+    },
+    computed: {
+      passwordValid() {
+        return this.userdata.password === this.passwordConfirm;
+      },
+      isDisabled() {
+        const { name, password, username, mail, usergroup } = this.userdata;
+        return (
+          this.showLoginWarning &&
+          (!name ||
+            !username ||
+            !password ||
+            !mail ||
+            !usergroup ||
+            !this.passwordValid)
+        );
+      }
     },
     methods: {
       ...mapActions("userStore", { registerNewUser: REGISTER_NEW_USER }),
@@ -96,9 +150,15 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ab accusamus d
         this.$refs.profilePicture = "";
       },
       signup() {
-        this.registerNewUser(this.userdata).then(
-          () => (this.loginSuccessful = true)
-        );
+        if (!this.passwordValid) return this.enableLoginWarning();
+        this.registerNewUser(this.userdata)
+          .then(() => (this.loginSuccessful = true))
+          .catch(this.enableLoginWarning);
+      },
+      async enableLoginWarning() {
+        this.redLoginButton = true;
+        await wait(200);
+        this.redLoginButton = false;
       }
     }
   };
