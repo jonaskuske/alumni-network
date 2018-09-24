91005bb753ba50c5dbfa921b39f5f831c77fb717
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-09-24 02:45:20 +0200

    build: enable gzip & brotli compression

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-/* global module*/
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const zopfli = require('@gfx/zopfli')
+const BrotliWebpackPlugin = require('brotli-webpack-plugin')
 
 module.exports = {
   lintOnSave: true,
@@ -8,5 +10,29 @@ module.exports = {
     themeColor: '#4040e9',
     msTileColor: '#4040e9',
     appleMobileWebAppStatusBarStyle: 'black',
+  },
+  chainWebpack: config => {
+    config.plugin('compress-gzip').use(CompressionWebpackPlugin, [
+      {
+        test: /\.(js|css|html|svg|json)$/,
+        threshold: 10240,
+        minRatio: 0.7,
+        cache: true,
+        compressionOptions: {
+          numiterations: 15,
+        },
+        algorithm(input, compressionOptions, callback) {
+          return zopfli.gzip(input, compressionOptions, callback)
+        },
+      },
+    ])
+
+    config.plugin('compress-brotli').use(BrotliWebpackPlugin, [
+      {
+        test: /\.(js|css|html|svg|json)$/,
+        threshold: 10240,
+        minRatio: 0.7,
+      },
+    ])
   },
 }
